de77362cb73378c72c038f2a539270897483ab8e
oasisgorilla <yongjae0522@gmail.com>
2024-05-08 23:28:26 +0900

    fix little bit

--- echo/csapp.c
+++ echo/csapp.c
@@ -1064,6 +1064,21 @@ int Open_listenfd(char *port)
     return rc;
 }
 
+/*echo함수 정의*/
+
+void echo(int connfd)
+{
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
+}
+
 /* $end csapp.c */
 
 
